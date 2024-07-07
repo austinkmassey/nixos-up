60c2126e737424b4cec2d5ddbbb7e7ed9e6005ad
Austin Massey <austinkmassey@gmail.com>
2024-07-06 22:14:13 -0500

    Changes how home-manager is called

--- sandbox/flake.nix
+++ sandbox/flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -23,19 +22,13 @@
     nixosConfigurations = {
       sandbox = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
-      };
-    };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#sandboxer@sandbox'
-    homeConfigurations = {
-      "sandboxer@sandbox" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [
+          ./nixos/configuration.nix
+	  home-manager.nixosModules.home-manager {
+            home-manager.useUserPackages = true;
+	    home-manager.users.sandboxer = import ./home-manager/home.nix
+	  }
+	];
       };
     };
   };
